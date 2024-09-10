d99b5be8705e97e18540d00a7bbeb74fe20aa063
Georgia <9707976+ggt3@users.noreply.github.com>
2024-09-10 11:39:44 -0400

    Fixing images

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css"
 import Header from "./components/Header";
-import Article1 from "./components/Article";
+import Article1 from "./components/Article1";
+import Article2 from "./components/Article2";
 import Footer from "./components/Footer";
 function App() {
     return (
         <main>
             <Header/>
             <Article1/>
+            <Article2/>
             <Footer/>
         </main>
     )
---
+++ src/components/Article2.jsx
@@ -0,0 +1,17 @@
+function Article2() {
+    return (
+        <article>
+
+            <time dateTime="2020-11-12">11/12/20</time>
+            <h2 className="title">On the Street In Brooklyn</h2>
+            <img src="./images/blog-image-2.jpg" alt="woman on street in Brooklyn"/>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vitae felis diam. Sed feugiat dui sed nisi faucibus dignissim. Suspendisse potenti. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed a pretium nunc. Duis aliquet ornare ex, vel facilisis nisi porttitor nec. Integer vestibulum vel arcu at tempus. Fusce porttitor quam nec nibh sagittis elementum. Etiam vel porttitor augue. Fusce id ante a nisi euismod tristique. Maecenas sit amet ante vel eros rutrum maximus nec sit amet ligula.
+
+              Ut sed arcu sed augue volutpat venenatis pellentesque quis orci. Nunc libero neque, tempor vel aliquam eget, interdum vitae urna. Curabitur quis urna sed turpis hendrerit laoreet. Etiam quis accumsan ante. Suspendisse imperdiet accumsan metus, non fermentum enim cursus at. Morbi et dolor convallis diam imperdiet ornare sed sed quam. Cras placerat est sit amet purus blandit, non rutrum leo commodo. Morbi vehicula sit amet velit id dictum.</p>
+              <a href="#" className="continue" >Continues...</a>
+
+        </article>
+    )
+}
+
+export default Article2;
